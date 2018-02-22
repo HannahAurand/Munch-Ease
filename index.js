c43435435b55251119eb5b08013daa7546fd3fd3
Timmy Le <le.timmy94@gmail.com>
2018-02-22 11:28:40 -0500

    include our controllers

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express()
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const recipesController = require('./controllers/recipes')
 
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+app.use('/recipes', recipesController)
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
+app.listen(3000, () => console.log('this is working'))
